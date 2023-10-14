8c84db86cc9470ee7eff29487169b0488b989651
gabec <gabec@Boogie_Machine>
2023-10-14 14:13:43 -0500

    Added a background to the start and load screen files as well as a load button and centered the buttons to look more visually appealing start and load menus appear to be completely functional in their current state.

--- src/shadows/of/the/forsaken/LoadScreen.java
+++ src/shadows/of/the/forsaken/LoadScreen.java
@@ -4,30 +4,67 @@
  */
 package shadows.of.the.forsaken;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 /**
  * [[insert description of load screen]]
  * @author Nathan Ainslie
+ * @author Gabe Carpenter
  */
 public class LoadScreen extends JPanel {
     private SceneSwitcher switcher;
     private final JButton backButton;
+    private ImageIcon backgroundImage;
     
     
     public LoadScreen(SceneSwitcher parent)
     {
         switcher = parent;
         
+        // Load the background image from the internet and resize it (code for background image is copied from StartScreen.java
+        try {
+            URL imageUrl = new URL("https://cdna.artstation.com/p/assets/images/images/006/315/366/large/taryn-meixner-dungeon-interior.jpg?1497628693");
+            BufferedImage originalImage = ImageIO.read(imageUrl);
+
+            int newWidth = 800;  // Set the width of the panel
+            int newHeight = 600; // Set the height of the panel
+
+            // Resize the image to fit within the panel dimensions
+            Image scaledImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+            backgroundImage = new ImageIcon(scaledImage); //New image resized to fit the 800 x 600 window
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception, e.g., show an error message, basically if improper url will throw error
+        }
+        
         // Create the components
-        backButton = new JButton("Start Game");
+        backButton = new JButton("Back to Start");
         
         // Add component functionality
         backButton.addActionListener((ActionEvent e) -> switcher.changeScene("START_SCREEN"));
         
         // Add components to the frame (note the order)
         this.add(backButton);
+    }
+    
+    // Override to allow image to be placed in background and to change positioning of the button(s)
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        backgroundImage.paintIcon(this, g, 0, 0);
+        
+        int buttonWidth = 150;
+        int buttonHeight = 50;
+        int panelWidth = getWidth(); // Get the panel's width
+        int panelHeight = getHeight(); // Get the panel's height
+        int buttonX = (panelWidth - buttonWidth) / 2;
+        int buttonY = (panelHeight - (-2)*buttonHeight) / 2;
+
+        backButton.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
     }
 }
--- src/shadows/of/the/forsaken/StartScreen.java
+++ src/shadows/of/the/forsaken/StartScreen.java
@@ -4,29 +4,78 @@
  */
 package shadows.of.the.forsaken;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.net.URL; //allows the program to work with urls
+import javax.imageio.ImageIO; //allows the program to read and write images
+import java.awt.image.BufferedImage; //allows the program to use image processing
 
 /**
  * [[insert description of start screen component]]
  * @author Nathan Ainslie
+ * @author Gabe Carpenter
  */
 public class StartScreen extends JPanel {
     private SceneSwitcher switcher;
     private final JButton startButton;
-    
-    
-    public StartScreen(SceneSwitcher parent)
-    {
+    private final JButton loadButton;
+    private ImageIcon backgroundImage;
+
+    public StartScreen(SceneSwitcher parent) {
         switcher = parent;
-        // Create the components
+
+        // Loads the background image from the internet and resizes it (figuring out how to import and resize the image done by chat gpt)
+        try {
+            URL imageUrl = new URL("https://cdna.artstation.com/p/assets/images/images/006/315/366/large/taryn-meixner-dungeon-interior.jpg?1497628693");
+            BufferedImage originalImage = ImageIO.read(imageUrl);
+
+            int newWidth = 800;  // Set the width of the panel
+            int newHeight = 600; // Set the height of the panel
+
+            // Resize the image to fit within the panel dimensions
+            Image scaledImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+            backgroundImage = new ImageIcon(scaledImage); //The new image resized to fit the 800 x 600 window
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception, e.g., show an error message, basically if improper url input will throw error
+        }
+
+        // Create the buttons
         startButton = new JButton("Start Game");
-        
+        loadButton = new JButton("Load Game");
+
         // Add component functionality
         startButton.addActionListener((ActionEvent e) -> switcher.changeScene("DUNGEON_MAP"));
+        loadButton.addActionListener((ActionEvent e) -> switcher.changeScene("LOAD_SCREEN"));
+
+        // Add the buttons to the panel
+        add(startButton);
+        add(loadButton);
+    }
+
+    // Use an override to allow the image to be in the background as well as changing button positions
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        backgroundImage.paintIcon(this, g, 0, 0);
         
-        // Add components to the frame (note the order)
-        this.add(startButton);
+        // Calculate the start button's position to center it
+        int buttonWidth = 150;
+        int buttonHeight = 50;
+        int panelWidth = getWidth(); // Get the panel's width
+        int panelHeight = getHeight(); // Get the panel's height
+        int buttonX = (panelWidth - buttonWidth) / 2; // Centers the button horizontally
+        int buttonY = (panelHeight - buttonHeight) / 2; // Centers the button vertically
+
+        startButton.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
+        
+        // Calculate the load button's position to place it slightly below the start button
+        int lbuttonWidth = 150;
+        int lbuttonHeight = 50;
+        int lbuttonX = (panelWidth - lbuttonWidth) / 2; // Centers the button horizontally
+        int lbuttonY = (panelHeight - (-2)*lbuttonHeight) / 2; // Slightly shifts the button down from vertical center
+        
+        loadButton.setBounds(lbuttonX, lbuttonY, lbuttonWidth, lbuttonHeight);
     }
 }
